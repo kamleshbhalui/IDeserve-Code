7069c6a0586574ca6b509b140ce47abd28d41062
Kamlesh Kumar <kamleshbhalui@gmail.com>
2017-09-06 11:46:26 +0530

    First Commit

---
+++ base.h
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<set>
+#include<unordered_map>
+using namespace std;
+#define start_main int main(int argc,char**argv){
+#define end_main return 0;}
