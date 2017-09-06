060299828e620c2a08f039136304f671429402f0
Kamlesh Kumar <kamleshbhalui@gmail.com>
2017-09-06 11:48:02 +0530

    Delete base.h

--- base.h
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<set>
-#include<unordered_map>
-using namespace std;
-#define start_main int main(int argc,char**argv){
-#define end_main return 0;}
